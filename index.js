f08faf8d1e7bc23d1ec53db554aaf1b6df6e9a21
anand <anandpromax@gmail.com>
2024-02-22 23:06:36 +0530

    basic setup

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const morgan = require("morgan");
 const app = express();
 const PORT = 3005;
-app.use(morgan('combined'));
-app.get('/home', (res, req) => {
-    req.send('Welcome to the home page');
+app.use(morgan("combined"));
+app.use(
+  "/bookingservice",
+  createProxyMiddleware({
+    target: "http://localhost:3000",
+    changeOrigin: true,
+  })
+);
+app.get("/home", (res, req) => {
+  req.send("Welcome to the home page");
 });
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-    
