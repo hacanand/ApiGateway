df5f02c973d3d67c27699338159301b9ed83dbc2
anand <anandpromax@gmail.com>
2024-02-23 00:51:52 +0530

    ratelimiter installed

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const morgan = require("morgan");
 const app = express();
 const PORT = 3005;
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+})
 app.use(morgan("combined"));
 app.use(
   "/bookingservice",
   createProxyMiddleware({
-    target: "http://localhost:3000",
+    target: "http://localhost:3005/home",
     changeOrigin: true,
   })
 );
